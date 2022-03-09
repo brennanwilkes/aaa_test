e7bac46850b614b3b2407db0dcc67ee70bfa442c
Brennan Wilkes <brennan@codexwilkes.com>
2022-03-09 00:23:56 -0800

    DevXP: Initialized Terraform

--- terraform.tf
+++ terraform.tf
@@ -12,40 +12,72 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_backend_bucket" {
-      bucket = "terraform-state-bvtmkafmqc7phc7uuk6zltekgohm29k4x8rjd0l0bfb78"
+      bucket = "terraform-state-b1uv4tnb6989ql4fi2l0yp09ng1d0zcbkwbjis2d0v6fw"
 }
 
-resource "aws_dynamodb_table" "asdfff" {
-      name = "asdfff"
-      hash_key = "fafdasdf"
+resource "aws_instance" "asdasdfas" {
+      ami = data.aws_ami.amazon_latest.id
+      instance_type = "t2.micro"
+      lifecycle {
+        ignore_changes = [ami]
+      }
+      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
+      associate_public_ip_address = true
+      vpc_security_group_ids = [aws_security_group.devxp_security_group.id]
+      iam_instance_profile = aws_iam_instance_profile.asdasdfas_iam_role_instance_profile.name
+}
+
+resource "aws_eip" "asdasdfas_eip" {
+      instance = aws_instance.asdasdfas.id
+      vpc = true
+}
+
+resource "aws_dynamodb_table" "asdf" {
+      name = "asdf"
+      hash_key = "asdf"
       billing_mode = "PAY_PER_REQUEST"
       ttl {
         attribute_name = "TimeToExist"
         enabled = true
       }
       attribute {
-        name = "fafdasdf"
+        name = "asdf"
         type = "S"
       }
 }
 
-resource "aws_iam_user" "asdfff_iam" {
-      name = "asdfff_iam"
+resource "aws_iam_user" "asdf_iam" {
+      name = "asdf_iam"
 }
 
-resource "aws_iam_user_policy_attachment" "asdfff_iam_policy_attachment0" {
-      user = aws_iam_user.asdfff_iam.name
-      policy_arn = aws_iam_policy.asdfff_iam_policy0.arn
+resource "aws_iam_user_policy_attachment" "asdf_iam_policy_attachment0" {
+      user = aws_iam_user.asdf_iam.name
+      policy_arn = aws_iam_policy.asdf_iam_policy0.arn
 }
 
-resource "aws_iam_policy" "asdfff_iam_policy0" {
-      name = "asdfff_iam_policy0"
+resource "aws_iam_policy" "asdf_iam_policy0" {
+      name = "asdf_iam_policy0"
       path = "/"
-      policy = data.aws_iam_policy_document.asdfff_iam_policy_document.json
+      policy = data.aws_iam_policy_document.asdf_iam_policy_document.json
 }
 
-resource "aws_iam_access_key" "asdfff_iam_access_key" {
-      user = aws_iam_user.asdfff_iam.name
+resource "aws_iam_access_key" "asdf_iam_access_key" {
+      user = aws_iam_user.asdf_iam.name
+}
+
+resource "aws_iam_instance_profile" "asdasdfas_iam_role_instance_profile" {
+      name = "asdasdfas_iam_role_instance_profile"
+      role = aws_iam_role.asdasdfas_iam_role.name
+}
+
+resource "aws_iam_role" "asdasdfas_iam_role" {
+      name = "asdasdfas_iam_role"
+      assume_role_policy = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": \"sts:AssumeRole\",\n      \"Principal\": {\n        \"Service\": \"ec2.amazonaws.com\"\n      },\n      \"Effect\": \"Allow\",\n      \"Sid\": \"\"\n    }\n  ]\n}"
+}
+
+resource "aws_iam_role_policy_attachment" "asdasdfas_iam_role_asdf_iam_policy0_attachment" {
+      policy_arn = aws_iam_policy.asdf_iam_policy0.arn
+      role = aws_iam_role.asdasdfas_iam_role.name
 }
 
 resource "aws_subnet" "devxp_vpc_subnet_public0" {
@@ -94,15 +126,38 @@ resource "aws_vpc" "devxp_vpc" {
 resource "aws_security_group" "devxp_security_group" {
       vpc_id = aws_vpc.devxp_vpc.id
       name = "devxp_security_group"
-      ingress = []
-      egress = []
+      ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+      egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
 }
 
-data "aws_iam_policy_document" "asdfff_iam_policy_document" {
+data "aws_ami" "amazon_latest" {
+      most_recent = true
+      owners = ["585441382316"]
+      filter {
+        name = "name"
+        values = ["*AmazonLinux*"]
+      }
+      filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+      }
+}
+
+data "aws_iam_policy_document" "asdf_iam_policy_document" {
       statement {
         actions = ["dynamodb:DescribeTable", "dynamodb:Query", "dynamodb:Scan", "dynamodb:BatchGet*", "dynamodb:DescribeStream", "dynamodb:DescribeTable", "dynamodb:Get*", "dynamodb:Query", "dynamodb:Scan", "dynamodb:BatchWrite*", "dynamodb:CreateTable", "dynamodb:Delete*", "dynamodb:Update*", "dynamodb:PutItem"]
         effect = "Allow"
-        resources = [aws_dynamodb_table.asdfff.arn]
+        resources = [aws_dynamodb_table.asdf.arn]
       }
       statement {
         actions = ["dynamodb:List*", "dynamodb:DescribeReservedCapacity*", "dynamodb:DescribeLimits", "dynamodb:DescribeTimeToLive"]
