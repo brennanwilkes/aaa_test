7d07c1f305bb35107f00d582a6537adaf2f3aa17
Brennan Wilkes <brennan@codexwilkes.com>
2022-03-17 08:45:30 -0700

    DevXP: Initialized Terraform

--- terraform.tf
+++ terraform.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_backend_bucket" {
-      bucket = "terraform-state-d2igw74qq5otg3bvgzlxeygwd21ywihtf0qhy1769rc2d"
+      bucket = "terraform-state-lifvuk794c2lk2e6dii59kdz76q28zc2mbqjck3x22jlr"
 }
 
 resource "aws_instance" "asdasdfas" {
@@ -21,7 +21,7 @@ resource "aws_instance" "asdasdfas" {
       lifecycle {
         ignore_changes = [ami]
       }
-      subnet_id = aws_subnet.devxp_vpc_subnet_public.id
+      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
       associate_public_ip_address = true
       vpc_security_group_ids = [aws_security_group.devxp_security_group.id]
       iam_instance_profile = aws_iam_instance_profile.asdasdfas_iam_role_instance_profile.name
@@ -30,6 +30,40 @@ resource "aws_instance" "asdasdfas" {
 resource "aws_eip" "asdasdfas_eip" {
       instance = aws_instance.asdasdfas.id
       vpc = true
+}
+
+resource "aws_dynamodb_table" "asdf" {
+      name = "asdf"
+      hash_key = "asdfDAFADF"
+      billing_mode = "PAY_PER_REQUEST"
+      ttl {
+        attribute_name = "TimeToExist"
+        enabled = true
+      }
+      attribute {
+        name = "asdfDAFADF"
+        type = "S"
+        _id = "62295acfb6f05f8866e135a6"
+      }
+}
+
+resource "aws_iam_user" "asdf_iam" {
+      name = "asdf_iam"
+}
+
+resource "aws_iam_user_policy_attachment" "asdf_iam_policy_attachment0" {
+      user = aws_iam_user.asdf_iam.name
+      policy_arn = aws_iam_policy.asdf_iam_policy0.arn
+}
+
+resource "aws_iam_policy" "asdf_iam_policy0" {
+      name = "asdf_iam_policy0"
+      path = "/"
+      policy = data.aws_iam_policy_document.asdf_iam_policy_document.json
+}
+
+resource "aws_iam_access_key" "asdf_iam_access_key" {
+      user = aws_iam_user.asdf_iam.name
 }
 
 resource "aws_iam_instance_profile" "asdasdfas_iam_role_instance_profile" {
@@ -42,27 +76,23 @@ resource "aws_iam_role" "asdasdfas_iam_role" {
       assume_role_policy = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": \"sts:AssumeRole\",\n      \"Principal\": {\n        \"Service\": \"ec2.amazonaws.com\"\n      },\n      \"Effect\": \"Allow\",\n      \"Sid\": \"\"\n    }\n  ]\n}"
 }
 
-resource "aws_subnet" "devxp_vpc_subnet_private" {
-      vpc_id = aws_vpc.devxp_vpc.id
-      cidr_block = "10.0.128.0/24"
-      map_public_ip_on_launch = false
-      availability_zone = "us-west-2a"
+resource "aws_iam_role_policy_attachment" "asdasdfas_iam_role_asdf_iam_policy0_attachment" {
+      policy_arn = aws_iam_policy.asdf_iam_policy0.arn
+      role = aws_iam_role.asdasdfas_iam_role.name
 }
 
-resource "aws_route_table" "devxp_vpc_routetable_priv" {
+resource "aws_subnet" "devxp_vpc_subnet_public0" {
       vpc_id = aws_vpc.devxp_vpc.id
-}
-
-resource "aws_route_table_association" "devxp_vpc_subnet_private_assoc" {
-      subnet_id = aws_subnet.devxp_vpc_subnet_private.id
-      route_table_id = aws_route_table.devxp_vpc_routetable_priv.id
-}
-
-resource "aws_subnet" "devxp_vpc_subnet_public" {
-      vpc_id = aws_vpc.devxp_vpc.id
-      cidr_block = "10.0.0.0/24"
+      cidr_block = "10.0.0.0/25"
       map_public_ip_on_launch = true
       availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "devxp_vpc_subnet_public1" {
+      vpc_id = aws_vpc.devxp_vpc.id
+      cidr_block = "10.0.128.0/25"
+      map_public_ip_on_launch = true
+      availability_zone = "us-west-2b"
 }
 
 resource "aws_internet_gateway" "devxp_vpc_internetgateway" {
@@ -84,7 +114,7 @@ resource "aws_route" "devxp_vpc_internet_route" {
 }
 
 resource "aws_route_table_association" "devxp_vpc_subnet_public_assoc" {
-      subnet_id = aws_subnet.devxp_vpc_subnet_public.id
+      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
       route_table_id = aws_route_table.devxp_vpc_routetable_pub.id
 }
 
@@ -98,17 +128,18 @@ resource "aws_security_group" "devxp_security_group" {
       vpc_id = aws_vpc.devxp_vpc.id
       name = "devxp_security_group"
       ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
       }
-      egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
+      ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
       }
+      egress = []
 }
 
 data "aws_ami" "amazon_latest" {
@@ -121,6 +152,19 @@ data "aws_ami" "amazon_latest" {
       filter {
         name = "virtualization-type"
         values = ["hvm"]
+      }
+}
+
+data "aws_iam_policy_document" "asdf_iam_policy_document" {
+      statement {
+        actions = ["dynamodb:DescribeTable", "dynamodb:Query", "dynamodb:Scan", "dynamodb:BatchGet*", "dynamodb:DescribeStream", "dynamodb:DescribeTable", "dynamodb:Get*", "dynamodb:Query", "dynamodb:Scan", "dynamodb:BatchWrite*", "dynamodb:CreateTable", "dynamodb:Delete*", "dynamodb:Update*", "dynamodb:PutItem"]
+        effect = "Allow"
+        resources = [aws_dynamodb_table.asdf.arn]
+      }
+      statement {
+        actions = ["dynamodb:List*", "dynamodb:DescribeReservedCapacity*", "dynamodb:DescribeLimits", "dynamodb:DescribeTimeToLive"]
+        effect = "Allow"
+        resources = ["*"]
       }
 }
 
