b542ebfade2ae3a673b7d6e22e559901824252ea
Brennan Wilkes <brennan@codexwilkes.com>
2022-04-11 18:12:27 -0700

    DevXP: Initialized Terraform

--- terraform.tf
+++ terraform.tf
@@ -1,21 +1,18 @@
 terraform {
   required_providers {
-    google =  {
-    source = "hashicorp/google"
-    version = ">= 4.10.0"
+    aws =  {
+    source = "hashicorp/aws"
+    version = ">= 2.7.0"
     }
   }
 }
 
-provider "google" {
-    project = "myproject-440810"
-    region = "us-west1"
+provider "aws" {
+    region = "us-west-2"
 }
 
-resource "google_storage_bucket" "terraform_backend_bucket" {
-      location = "us-west1"
-      name = "terraform-state-5sn7kgg91q9dzpvoiyb80rryiop7jignano2bjdb8iny5"
-      project = "myproject-440810"
+resource "aws_s3_bucket" "terraform_backend_bucket" {
+      bucket = "terraform-state-dwz7yaaexi9yft4zrqa95mvfpgaybzyd07a6rsngxl27a"
 }
 
 
