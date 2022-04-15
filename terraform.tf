11a544b9a728e1376e1a33037e3c99d639d229ef
Brennan Wilkes <brennan@codexwilkes.com>
2022-04-14 18:27:21 -0700

    DevXP: Initialized Terraform

--- terraform.tf
+++ terraform.tf
@@ -51,42 +51,6 @@ resource "aws_iam_access_key" "Instance-tdex_iam_access_key" {
       user = aws_iam_user.Instance-tdex_iam.name
 }
 
-resource "aws_instance" "Instance-acwa-a" {
-      ami = data.aws_ami.amazon_latest.id
-      instance_type = "t2.medium"
-      lifecycle {
-        ignore_changes = [ami]
-      }
-      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
-      associate_public_ip_address = true
-      vpc_security_group_ids = [aws_security_group.devxp_security_group.id]
-      iam_instance_profile = aws_iam_instance_profile.Instance-acwa-a_iam_role_instance_profile.name
-}
-
-resource "aws_eip" "Instance-acwa-a_eip" {
-      vpc = true
-      instance = aws_instance.Instance-acwa-a.id
-}
-
-resource "aws_iam_user" "Instance-acwa-a_iam" {
-      name = "Instance-acwa-a_iam"
-}
-
-resource "aws_iam_user_policy_attachment" "Instance-acwa-a_iam_policy_attachment0" {
-      user = aws_iam_user.Instance-acwa-a_iam.name
-      policy_arn = aws_iam_policy.Instance-acwa-a_iam_policy0.arn
-}
-
-resource "aws_iam_policy" "Instance-acwa-a_iam_policy0" {
-      name = "Instance-acwa-a_iam_policy0"
-      path = "/"
-      policy = data.aws_iam_policy_document.Instance-acwa-a_iam_policy_document.json
-}
-
-resource "aws_iam_access_key" "Instance-acwa-a_iam_access_key" {
-      user = aws_iam_user.Instance-acwa-a_iam.name
-}
-
 resource "aws_instance" "Instance-acwa-c" {
       ami = data.aws_ami.amazon_latest.id
       instance_type = "t2.medium"
@@ -123,14 +87,45 @@ resource "aws_iam_access_key" "Instance-acwa-c_iam_access_key" {
       user = aws_iam_user.Instance-acwa-c_iam.name
 }
 
+resource "aws_instance" "Instance-acwa-a" {
+      ami = data.aws_ami.amazon_latest.id
+      instance_type = "t2.medium"
+      lifecycle {
+        ignore_changes = [ami]
+      }
+      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
+      associate_public_ip_address = true
+      vpc_security_group_ids = [aws_security_group.devxp_security_group.id]
+      iam_instance_profile = aws_iam_instance_profile.Instance-acwa-a_iam_role_instance_profile.name
+}
+
+resource "aws_eip" "Instance-acwa-a_eip" {
+      vpc = true
+      instance = aws_instance.Instance-acwa-a.id
+}
+
+resource "aws_iam_user" "Instance-acwa-a_iam" {
+      name = "Instance-acwa-a_iam"
+}
+
+resource "aws_iam_user_policy_attachment" "Instance-acwa-a_iam_policy_attachment0" {
+      user = aws_iam_user.Instance-acwa-a_iam.name
+      policy_arn = aws_iam_policy.Instance-acwa-a_iam_policy0.arn
+}
+
+resource "aws_iam_policy" "Instance-acwa-a_iam_policy0" {
+      name = "Instance-acwa-a_iam_policy0"
+      path = "/"
+      policy = data.aws_iam_policy_document.Instance-acwa-a_iam_policy_document.json
+}
+
+resource "aws_iam_access_key" "Instance-acwa-a_iam_access_key" {
+      user = aws_iam_user.Instance-acwa-a_iam.name
+}
+
 resource "aws_iam_instance_profile" "Instance-tdex_iam_role_instance_profile" {
       name = "Instance-tdex_iam_role_instance_profile"
       role = aws_iam_role.Instance-tdex_iam_role.name
-}
-
-resource "aws_iam_instance_profile" "Instance-acwa-a_iam_role_instance_profile" {
-      name = "Instance-acwa-a_iam_role_instance_profile"
-      role = aws_iam_role.Instance-acwa-a_iam_role.name
 }
 
 resource "aws_iam_instance_profile" "Instance-acwa-c_iam_role_instance_profile" {
@@ -138,18 +133,23 @@ resource "aws_iam_instance_profile" "Instance-acwa-c_iam_role_instance_profile" 
       role = aws_iam_role.Instance-acwa-c_iam_role.name
 }
 
+resource "aws_iam_instance_profile" "Instance-acwa-a_iam_role_instance_profile" {
+      name = "Instance-acwa-a_iam_role_instance_profile"
+      role = aws_iam_role.Instance-acwa-a_iam_role.name
+}
+
 resource "aws_iam_role" "Instance-tdex_iam_role" {
       name = "Instance-tdex_iam_role"
       assume_role_policy = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": \"sts:AssumeRole\",\n      \"Principal\": {\n        \"Service\": \"ec2.amazonaws.com\"\n      },\n      \"Effect\": \"Allow\",\n      \"Sid\": \"\"\n    }\n  ]\n}"
 }
 
-resource "aws_iam_role" "Instance-acwa-a_iam_role" {
-      name = "Instance-acwa-a_iam_role"
+resource "aws_iam_role" "Instance-acwa-c_iam_role" {
+      name = "Instance-acwa-c_iam_role"
       assume_role_policy = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": \"sts:AssumeRole\",\n      \"Principal\": {\n        \"Service\": \"ec2.amazonaws.com\"\n      },\n      \"Effect\": \"Allow\",\n      \"Sid\": \"\"\n    }\n  ]\n}"
 }
 
-resource "aws_iam_role" "Instance-acwa-c_iam_role" {
-      name = "Instance-acwa-c_iam_role"
+resource "aws_iam_role" "Instance-acwa-a_iam_role" {
+      name = "Instance-acwa-a_iam_role"
       assume_role_policy = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": \"sts:AssumeRole\",\n      \"Principal\": {\n        \"Service\": \"ec2.amazonaws.com\"\n      },\n      \"Effect\": \"Allow\",\n      \"Sid\": \"\"\n    }\n  ]\n}"
 }
 
@@ -245,19 +245,6 @@ data "aws_ami" "amazon_latest" {
       }
 }
 
-data "aws_iam_policy_document" "Instance-acwa-a_iam_policy_document" {
-      statement {
-        actions = ["ec2:RunInstances", "ec2:AssociateIamInstanceProfile", "ec2:ReplaceIamInstanceProfileAssociation"]
-        effect = "Allow"
-        resources = ["arn:aws:ec2:::*"]
-      }
-      statement {
-        actions = ["iam:PassRole"]
-        effect = "Allow"
-        resources = [aws_instance.Instance-acwa-a.arn]
-      }
-}
-
 data "aws_iam_policy_document" "Instance-acwa-c_iam_policy_document" {
       statement {
         actions = ["ec2:RunInstances", "ec2:AssociateIamInstanceProfile", "ec2:ReplaceIamInstanceProfileAssociation"]
@@ -268,6 +255,19 @@ data "aws_iam_policy_document" "Instance-acwa-c_iam_policy_document" {
         actions = ["iam:PassRole"]
         effect = "Allow"
         resources = [aws_instance.Instance-acwa-c.arn]
+      }
+}
+
+data "aws_iam_policy_document" "Instance-acwa-a_iam_policy_document" {
+      statement {
+        actions = ["ec2:RunInstances", "ec2:AssociateIamInstanceProfile", "ec2:ReplaceIamInstanceProfileAssociation"]
+        effect = "Allow"
+        resources = ["arn:aws:ec2:::*"]
+      }
+      statement {
+        actions = ["iam:PassRole"]
+        effect = "Allow"
+        resources = [aws_instance.Instance-acwa-a.arn]
       }
 }
 
