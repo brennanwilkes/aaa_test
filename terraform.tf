2beb3546b3b6e0a86a7983dd5ab3b2a3a53954f8
Brennan Wilkes <brennan@codexwilkes.com>
2022-06-04 13:41:07 -0700

    DevXP: Initialized Terraform

--- terraform.tf
+++ terraform.tf
@@ -21,6 +21,15 @@ resource "aws_instance" "Instance-ihul" {
       lifecycle {
         ignore_changes = [ami]
       }
+      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
+      associate_public_ip_address = true
+      vpc_security_group_ids = [aws_security_group.devxp_security_group.id]
+      iam_instance_profile = aws_iam_instance_profile.Instance-ihul_iam_role_instance_profile.name
+}
+
+resource "aws_eip" "Instance-ihul_eip" {
+      vpc = true
+      instance = aws_instance.Instance-ihul.id
 }
 
 resource "aws_iam_user" "Instance-ihul_iam" {
@@ -40,6 +49,82 @@ resource "aws_iam_policy" "Instance-ihul_iam_policy0" {
 
 resource "aws_iam_access_key" "Instance-ihul_iam_access_key" {
       user = aws_iam_user.Instance-ihul_iam.name
+}
+
+resource "aws_iam_instance_profile" "Instance-ihul_iam_role_instance_profile" {
+      name = "Instance-ihul_iam_role_instance_profile"
+      role = aws_iam_role.Instance-ihul_iam_role.name
+}
+
+resource "aws_iam_role" "Instance-ihul_iam_role" {
+      name = "Instance-ihul_iam_role"
+      assume_role_policy = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": \"sts:AssumeRole\",\n      \"Principal\": {\n        \"Service\": \"ec2.amazonaws.com\"\n      },\n      \"Effect\": \"Allow\",\n      \"Sid\": \"\"\n    }\n  ]\n}"
+}
+
+resource "aws_subnet" "devxp_vpc_subnet_public0" {
+      vpc_id = aws_vpc.devxp_vpc.id
+      cidr_block = "10.0.0.0/25"
+      map_public_ip_on_launch = true
+      availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "devxp_vpc_subnet_public1" {
+      vpc_id = aws_vpc.devxp_vpc.id
+      cidr_block = "10.0.128.0/25"
+      map_public_ip_on_launch = true
+      availability_zone = "us-west-2b"
+}
+
+resource "aws_internet_gateway" "devxp_vpc_internetgateway" {
+      vpc_id = aws_vpc.devxp_vpc.id
+}
+
+resource "aws_route_table" "devxp_vpc_routetable_pub" {
+      route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.devxp_vpc_internetgateway.id
+      }
+      vpc_id = aws_vpc.devxp_vpc.id
+}
+
+resource "aws_route" "devxp_vpc_internet_route" {
+      route_table_id = aws_route_table.devxp_vpc_routetable_pub.id
+      destination_cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.devxp_vpc_internetgateway.id
+}
+
+resource "aws_route_table_association" "devxp_vpc_subnet_public_assoc" {
+      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
+      route_table_id = aws_route_table.devxp_vpc_routetable_pub.id
+}
+
+resource "aws_vpc" "devxp_vpc" {
+      cidr_block = "10.0.0.0/16"
+      enable_dns_support = true
+      enable_dns_hostnames = true
+}
+
+resource "aws_security_group" "devxp_security_group" {
+      vpc_id = aws_vpc.devxp_vpc.id
+      name = "devxp_security_group"
+      ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+      egress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+      egress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
 }
 
 data "aws_iam_policy_document" "Instance-ihul_iam_policy_document" {
