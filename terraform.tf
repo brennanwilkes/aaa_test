4088e4dbf25c78e87527d8bd37af64a3f8091ef6
Brennan Wilkes <brennan@codexwilkes.com>
2022-06-04 12:50:30 -0700

    DevXP: Initialized Terraform

--- terraform.tf
+++ terraform.tf
@@ -12,122 +12,37 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_backend_bucket" {
-      bucket = "terraform-state-bcztk2ix0f21vqzwu4890hj9zvgcrlthdtoxn4fx2u2be"
+      bucket = "terraform-state-5ebz1s3t342gpli3ozxh4x04i7gw8uqfywgz2k88yy0cx"
 }
 
-resource "aws_instance" "Instance-aphh" {
+resource "aws_instance" "Instance-qteh" {
       ami = data.aws_ami.ubuntu_latest.id
       instance_type = "t2.micro"
       lifecycle {
         ignore_changes = [ami]
       }
-      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
-      associate_public_ip_address = true
-      vpc_security_group_ids = [aws_security_group.devxp_security_group.id]
-      iam_instance_profile = aws_iam_instance_profile.Instance-aphh_iam_role_instance_profile.name
 }
 
-resource "aws_eip" "Instance-aphh_eip" {
-      vpc = true
-      instance = aws_instance.Instance-aphh.id
+resource "aws_iam_user" "Instance-qteh_iam" {
+      name = "Instance-qteh_iam"
 }
 
-resource "aws_iam_user" "Instance-aphh_iam" {
-      name = "Instance-aphh_iam"
+resource "aws_iam_user_policy_attachment" "Instance-qteh_iam_policy_attachment0" {
+      user = aws_iam_user.Instance-qteh_iam.name
+      policy_arn = aws_iam_policy.Instance-qteh_iam_policy0.arn
 }
 
-resource "aws_iam_user_policy_attachment" "Instance-aphh_iam_policy_attachment0" {
-      user = aws_iam_user.Instance-aphh_iam.name
-      policy_arn = aws_iam_policy.Instance-aphh_iam_policy0.arn
-}
-
-resource "aws_iam_policy" "Instance-aphh_iam_policy0" {
-      name = "Instance-aphh_iam_policy0"
+resource "aws_iam_policy" "Instance-qteh_iam_policy0" {
+      name = "Instance-qteh_iam_policy0"
       path = "/"
-      policy = data.aws_iam_policy_document.Instance-aphh_iam_policy_document.json
+      policy = data.aws_iam_policy_document.Instance-qteh_iam_policy_document.json
 }
 
-resource "aws_iam_access_key" "Instance-aphh_iam_access_key" {
-      user = aws_iam_user.Instance-aphh_iam.name
+resource "aws_iam_access_key" "Instance-qteh_iam_access_key" {
+      user = aws_iam_user.Instance-qteh_iam.name
 }
 
-resource "aws_iam_instance_profile" "Instance-aphh_iam_role_instance_profile" {
-      name = "Instance-aphh_iam_role_instance_profile"
-      role = aws_iam_role.Instance-aphh_iam_role.name
-}
-
-resource "aws_iam_role" "Instance-aphh_iam_role" {
-      name = "Instance-aphh_iam_role"
-      assume_role_policy = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": \"sts:AssumeRole\",\n      \"Principal\": {\n        \"Service\": \"ec2.amazonaws.com\"\n      },\n      \"Effect\": \"Allow\",\n      \"Sid\": \"\"\n    }\n  ]\n}"
-}
-
-resource "aws_subnet" "devxp_vpc_subnet_public0" {
-      vpc_id = aws_vpc.devxp_vpc.id
-      cidr_block = "10.0.0.0/25"
-      map_public_ip_on_launch = true
-      availability_zone = "us-west-2a"
-}
-
-resource "aws_subnet" "devxp_vpc_subnet_public1" {
-      vpc_id = aws_vpc.devxp_vpc.id
-      cidr_block = "10.0.128.0/25"
-      map_public_ip_on_launch = true
-      availability_zone = "us-west-2b"
-}
-
-resource "aws_internet_gateway" "devxp_vpc_internetgateway" {
-      vpc_id = aws_vpc.devxp_vpc.id
-}
-
-resource "aws_route_table" "devxp_vpc_routetable_pub" {
-      route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.devxp_vpc_internetgateway.id
-      }
-      vpc_id = aws_vpc.devxp_vpc.id
-}
-
-resource "aws_route" "devxp_vpc_internet_route" {
-      route_table_id = aws_route_table.devxp_vpc_routetable_pub.id
-      destination_cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.devxp_vpc_internetgateway.id
-}
-
-resource "aws_route_table_association" "devxp_vpc_subnet_public_assoc" {
-      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
-      route_table_id = aws_route_table.devxp_vpc_routetable_pub.id
-}
-
-resource "aws_vpc" "devxp_vpc" {
-      cidr_block = "10.0.0.0/16"
-      enable_dns_support = true
-      enable_dns_hostnames = true
-}
-
-resource "aws_security_group" "devxp_security_group" {
-      vpc_id = aws_vpc.devxp_vpc.id
-      name = "devxp_security_group"
-      ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
-      egress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
-      egress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
-}
-
-data "aws_iam_policy_document" "Instance-aphh_iam_policy_document" {
+data "aws_iam_policy_document" "Instance-qteh_iam_policy_document" {
       statement {
         actions = ["ec2:RunInstances", "ec2:AssociateIamInstanceProfile", "ec2:ReplaceIamInstanceProfileAssociation"]
         effect = "Allow"
@@ -136,7 +51,7 @@ data "aws_iam_policy_document" "Instance-aphh_iam_policy_document" {
       statement {
         actions = ["iam:PassRole"]
         effect = "Allow"
-        resources = [aws_instance.Instance-aphh.arn]
+        resources = [aws_instance.Instance-qteh.arn]
       }
 }
 
