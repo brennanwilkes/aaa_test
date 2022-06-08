f08881e1b277807081d645d49961b15de417a768
Brennan Wilkes <brennan@codexwilkes.com>
2022-06-07 19:22:38 -0700

    DevXP: Initialized Terraform

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform = {
+  backend = {
+    s3 = {
+      bucket = "terraform-state-9rxwi9n13v5pkqmddykjimrfr91pvxzl6l81ohjfhv0an"
+      key = "terraform/state"
+      region = "us-west-2"
+}
+
