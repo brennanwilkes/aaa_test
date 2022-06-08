bc12390d1e30257e0bcf42a28c214aed9ec20493
Brennan Wilkes <brennan@codexwilkes.com>
2022-06-07 19:54:46 -0700

    DevXP: Initialized Terraform

--- backend.tf
+++ backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket = "terraform-state-9rxwi9n13v5pkqmddykjimrfr91pvxzl6l81ohjfhv0an"
     key = "terraform/state"
     region = "us-west-2"
-
   }
 }
--- terraform.tf
+++ terraform.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws =  {
-    source = "hashicorp/aws"
-    version = ">= 2.7.0"
-      }
-  }
-}
-  backend = {
-    s3 = {
-      bucket = "terraform-state-9rxwi9n13v5pkqmddykjimrfr91pvxzl6l81ohjfhv0an"
-      key = "terraform/state"
-      region = "us-west-2"
-}
-
 provider "aws" {
     region = "us-west-2"
 }
