8a2d2cb244ab651a07928cafc208f9cc70c7ba81
Brennan Wilkes <brennan@codexwilkes.com>
2022-06-07 22:57:41 -0700

    DevXP: Initialized Terraform

--- backend.tf
+++ backend.tf
@@ -10,4 +10,4 @@ terraform {
     }
   }
 }
-m
+
--- terraform.tf
+++ terraform.tf
@@ -1,59 +1,13 @@
 terraform = {
-  backend = {
-    s3 = {
+  backend "s3" {
       bucket = "terraform-state-9rxwi9n13v5pkqmddykjimrfr91pvxzl6l81ohjfhv0an"
       key = "terraform/state"
       region = "us-west-2"
+  }
 }
 
 provider "aws" {
     region = "us-west-2"
-}
-
-resource "aws_instance" "Instance-ifph" {
-      ami = data.aws_ami.ubuntu_latest.id
-      instance_type = "t2.micro"
-      lifecycle {
-        ignore_changes = [ami]
-      }
-      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
-      associate_public_ip_address = true
-      vpc_security_group_ids = [aws_security_group.devxp_security_group.id]
-      iam_instance_profile = aws_iam_instance_profile.Instance-ifph_iam_role_instance_profile.name
-}
-
-resource "aws_eip" "Instance-ifph_eip" {
-      vpc = true
-      instance = aws_instance.Instance-ifph.id
-}
-
-resource "aws_iam_user" "Instance-ifph_iam" {
-      name = "Instance-ifph_iam"
-}
-
-resource "aws_iam_user_policy_attachment" "Instance-ifph_iam_policy_attachment0" {
-      user = aws_iam_user.Instance-ifph_iam.name
-      policy_arn = aws_iam_policy.Instance-ifph_iam_policy0.arn
-}
-
-resource "aws_iam_policy" "Instance-ifph_iam_policy0" {
-      name = "Instance-ifph_iam_policy0"
-      path = "/"
-      policy = data.aws_iam_policy_document.Instance-ifph_iam_policy_document.json
-}
-
-resource "aws_iam_access_key" "Instance-ifph_iam_access_key" {
-      user = aws_iam_user.Instance-ifph_iam.name
-}
-
-resource "aws_iam_instance_profile" "Instance-ifph_iam_role_instance_profile" {
-      name = "Instance-ifph_iam_role_instance_profile"
-      role = aws_iam_role.Instance-ifph_iam_role.name
-}
-
-resource "aws_iam_role" "Instance-ifph_iam_role" {
-      name = "Instance-ifph_iam_role"
-      assume_role_policy = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": \"sts:AssumeRole\",\n      \"Principal\": {\n        \"Service\": \"ec2.amazonaws.com\"\n      },\n      \"Effect\": \"Allow\",\n      \"Sid\": \"\"\n    }\n  ]\n}"
 }
 
 resource "aws_subnet" "devxp_vpc_subnet_public0" {
@@ -108,6 +62,18 @@ resource "aws_security_group" "devxp_security_group" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
       }
+      ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+      ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
       egress {
         from_port = 80
         to_port = 80
@@ -122,31 +88,6 @@ resource "aws_security_group" "devxp_security_group" {
       }
 }
 
-data "aws_iam_policy_document" "Instance-ifph_iam_policy_document" {
-      statement {
-        actions = ["ec2:RunInstances", "ec2:AssociateIamInstanceProfile", "ec2:ReplaceIamInstanceProfileAssociation"]
-        effect = "Allow"
-        resources = ["arn:aws:ec2:::*"]
-      }
-      statement {
-        actions = ["iam:PassRole"]
-        effect = "Allow"
-        resources = [aws_instance.Instance-ifph.arn]
-      }
-}
-
-data "aws_ami" "ubuntu_latest" {
-      most_recent = true
-      owners = ["099720109477"]
-      filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64*"]
-      }
-      filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-      }
-}
 
 
 
