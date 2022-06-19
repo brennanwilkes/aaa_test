bfd15a2ef0d80cff57d8159301fec872597d3978
Brennan Wilkes <brennan@codexwilkes.com>
2022-06-18 22:36:03 -0700

    DevXP: Configured Terraform

--- devxp_main.tf
+++ devxp_main.tf
@@ -14,38 +14,6 @@ resource "aws_instance" "Instance-mtdw" {
       }
 }
 
-resource "aws_iam_user" "Instance-mtdw_iam" {
-      name = "Instance-mtdw_iam"
-}
-
-resource "aws_iam_user_policy_attachment" "Instance-mtdw_iam_policy_attachment0" {
-      user = aws_iam_user.Instance-mtdw_iam.name
-      policy_arn = aws_iam_policy.Instance-mtdw_iam_policy0.arn
-}
-
-resource "aws_iam_policy" "Instance-mtdw_iam_policy0" {
-      name = "Instance-mtdw_iam_policy0"
-      path = "/"
-      policy = data.aws_iam_policy_document.Instance-mtdw_iam_policy_document.json
-}
-
-resource "aws_iam_access_key" "Instance-mtdw_iam_access_key" {
-      user = aws_iam_user.Instance-mtdw_iam.name
-}
-
-data "aws_iam_policy_document" "Instance-mtdw_iam_policy_document" {
-      statement {
-        actions = ["ec2:RunInstances", "ec2:AssociateIamInstanceProfile", "ec2:ReplaceIamInstanceProfileAssociation"]
-        effect = "Allow"
-        resources = ["arn:aws:ec2:::*"]
-      }
-      statement {
-        actions = ["iam:PassRole"]
-        effect = "Allow"
-        resources = [aws_instance.Instance-mtdw.arn]
-      }
-}
-
 data "aws_ami" "ubuntu_latest" {
       most_recent = true
       owners = ["099720109477"]
